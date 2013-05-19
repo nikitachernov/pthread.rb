d6fe651921073dad01e4e4115f37f23f88718f8d
Nikita Cernovs <nikita.cernovs@gmail.com>
2013-05-19 16:51:37 +0300

    Documented source

--- lib/pthread/pthread.rb
+++ lib/pthread/pthread.rb
@@ -1,43 +1,100 @@
 require 'rinda/tuplespace'
 
-class Pthread::Pthread
-  @@ts  = Rinda::TupleSpace.new
-  @@pids = []
+module Pthread
 
-  def self.start_service(host)
-    @@host = host
-    DRb.start_service("druby://#{@@host}", @@ts)
-  end
+  # The +Pthread+ class is the main class that users work with.
+  # It used for creating forks to be executed in a separate processes
+  # including other machines
+  class Pthread
 
-  def self.add_executors(count = 1, queue=nil)
-    count.times do
-      @@pids << fork do
-        DRb.stop_service
-        Pthread::PthreadExecutor.new(@@host, queue)
+    @@ts   = Rinda::TupleSpace.new
+    @@pids = []
+
+    # Starts the drb server
+    #
+    # @param [ String ] that contains host url and port
+    #
+    # @example Start service
+    #   Pthread::Pthread.start_service '192.168.1.100:12345'
+    def self.start_service(host)
+      @@host = host
+      DRb.start_service("druby://#{@@host}", @@ts)
+    end
+
+    # Adds executors on the same machine as the main programm
+    #
+    # @param [ FixNum ] amount of executors to start
+    # @param [ Symbol, String ] name of the queue for executors to be attached to
+    #
+    # @example Add executors without a queue
+    #   Pthread::Pthread.add_executors 5
+    # @example Add executors for specific queue
+    #   Pthread::Pthread.add_executors 5, :tasks
+
+    def self.add_executors(count = 1, queue=nil)
+      count.times do
+        @@pids << fork do
+          DRb.stop_service
+          Pthread::PthreadExecutor.new(@@host, queue)
+        end
       end
     end
-  end
 
-  def self.add_executor(queue=nil)
-    add_executors(1, queue)
-  end
+    # Adds a single executor on the same machine as the main programm
+    #
+    # @param [ Symbol, String ] name of the queue for executor to be attached to
+    #
+    # @example Add executor without a queue
+    #   Pthread::Pthread.add_executor
+    # @example Add executor for specific queue
+    #   Pthread::Pthread.add_executor, :tasks
+    def self.add_executor(queue=nil)
+      add_executors(1, queue)
+    end
 
-  def self.kill_executors
-    Process.kill 'HUP', *@@pids
-    @@pids = []
-  end
+    # Kills all launched executors on this machine
+    #
+    # @example Add executors without a queue
+    #   Pthread::Pthread.kill_executors
+    def self.kill_executors
+      Process.kill 'HUP', *@@pids
+      @@pids = []
+    end
 
-  def initialize(job)
-    @@ts.write([self.object_id, job[:queue], job[:code], job[:context]])
-  end
+    # Initializes new pthread and schedules execution of the job
+    #
+    # @param [ Hash ] should containt :code, :context and optionally :queue
+    # 
+    # @example Initialize new parrallel job
+    #   pthread = Pthread::Pthread.new queue: 'tasks', code: %{
+    #     x ** 2
+    #   }, context: { x: 5 }
+    def initialize(job)
+      @@ts.write([self.object_id, job[:queue], job[:code], job[:context]])
+    end
 
-  def value
-    raw_value.is_a?(StandardError) ? raise(raw_value) : raw_value
-  end
 
-private
+    # Returns value of a pthread
+    #
+    # @note If value if not yet calculated will block the execution
+    # @note If pthread resulted in an exception it will be raised
+    #
+    # @example pthread.value
+    #
+    # @return [ Object ] value of a pthread
+    def value
+      raw_value.is_a?(StandardError) ? raise(raw_value) : raw_value
+    end
 
-  def raw_value
-    @raw_value ||= @@ts.take([self.object_id, nil])[1]
+  private
+
+    # Returns raw value of a pthread even if it was an exception
+    #
+    # @note If value if not yet calculated will block the execution
+    #
+    # @return [ Object ] raw value of a pthread    
+    def raw_value
+      @raw_value ||= @@ts.take([self.object_id, nil])[1]
+    end
   end
 end
--- lib/pthread/pthread_executor.rb
+++ lib/pthread/pthread_executor.rb
@@ -1,27 +1,40 @@
 require 'drb/drb'
 
-class Pthread::PthreadExecutor
-  def initialize(host, queue=nil)
-    DRb.start_service
-    ts = DRbObject.new_with_uri("druby://#{host}")
+module Pthread
 
-    loop do
-      pthread_id, _, code, context = ts.take([nil, queue, nil, nil])
+  # +PthreadExecutor+ is used by Pthread to run code in a separate fork.
+  # Users can you this class to start executors manually on remote machines.
+  class PthreadExecutor
 
-      context && context.each do |a, v|
-        singleton_class.class_eval { attr_accessor a }
-        self.send("#{a}=", context[a])
+    # Initliazes new executor.
+    #
+    # @param [ String ] DRB host that has main programm running
+    # @param [ String, Symbol ] optinal queue name to attach executor
+    #
+    # @example Connect to remote Drb service
+    #   Pthread::PthreadExecutor.new '192.168.1.100:12345', :tasks
+    def initialize(host, queue=nil)
+      DRb.start_service
+      ts = DRbObject.new_with_uri("druby://#{host}")
+
+      loop do
+        pthread_id, _, code, context = ts.take([nil, queue, nil, nil])
+
+        context && context.each do |a, v|
+          singleton_class.class_eval { attr_accessor a }
+          self.send("#{a}=", context[a])
+        end
+
+        value = begin
+          eval(code)
+        rescue => e
+          e
+        end
+
+        ts.write([pthread_id, value])
       end
-
-      value = begin
-        eval(code)
-      rescue => e
-        e
-      end
-
-      ts.write([pthread_id, value])
+    rescue DRb::DRbConnError
+      exit 0
     end
-  rescue DRb::DRbConnError
-    exit 0
   end
 end
